9fca9c51d052726761d049471d41d13dcaee765b
matthew.lau <matthew.lau@credera.co.uk>
2023-06-03 21:37:07 +0100

    feat: add scaffolding for Python container

---
+++ manage.py
@@ -0,0 +1,7 @@
+from src import app
+
+def runserver():
+    app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=True)
+
+if __name__ == "__main__":
+    runserver()
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name="pythong-fear-greed-api")
