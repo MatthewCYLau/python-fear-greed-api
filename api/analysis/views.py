ce30fe729486cad326ee83972b444e5ab86741f5
matthew.lau <matthew.lau@credera.co.uk>
2024-02-20 16:58:25 +0000

    feat: yfinance api

--- api/analysis/views.py
+++ api/analysis/views.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 import logging
-from api.util.util import (
-    generate_response,
-)
+import yfinance as yf
 from api.auth.auth import auth_required
+from api.exception.models import BadRequestException
+
 
 bp = Blueprint("analysis", __name__)
 
@@ -11,9 +11,14 @@ bp = Blueprint("analysis", __name__)
 @bp.route("/analysis", methods=(["GET"]))
 @auth_required
 def get_stock_analysis(_):
+    stock_symbol = request.args.get("stock", default=None, type=None)
+    if not stock_symbol:
+        raise BadRequestException("Provide a stock symbol", status_code=400)
+    logging.info(f"Analysing stock with ticker symbol {stock_symbol}...")
     try:
-        analysis = "Ok"
-        return generate_response(analysis)
+        data = yf.Ticker(stock_symbol)
+        logging.info(data.history(period="1mo"))
+        return "Ok"
     except Exception as e:
         logging.error(e)
         return jsonify({"message": "Get stock analysis failed"}), 500
