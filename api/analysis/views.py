e57c2233c31857d777e18b97e9cc9f3e93c6b5ac
matthew.lau <matthew.lau@credera.co.uk>
2024-02-22 19:24:58 +0000

    feat: analysis stock fair value

--- api/analysis/views.py
+++ api/analysis/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from api.db.setup import db
 import logging
 import yfinance as yf
 from api.auth.auth import auth_required
@@ -19,8 +20,24 @@ def get_stock_analysis(_):
         data = yf.Ticker(stock_symbol)
         df = data.history(period="1mo")
         most_recent_close = df.tail(1)["Close"].values[0]
-        most_recent_close = "{:.2f}".format(most_recent_close)
-        return jsonify({"close": most_recent_close}), 200
+        most_recent_close = float("{:.2f}".format(most_recent_close))
+        most_recent_fear_greed_index = int(
+            list(db["records"].find().sort("created", -1).limit(0))[0]["index"]
+        )
+        fair_value = round(
+            most_recent_close * ((100 - most_recent_fear_greed_index) / 100), 2
+        )
+        return (
+            jsonify(
+                {
+                    "stock": stock_symbol,
+                    "close": most_recent_close,
+                    "mostRecentFearGreedIndex": most_recent_fear_greed_index,
+                    "fairValue": fair_value,
+                }
+            ),
+            200,
+        )
     except Exception as e:
         logging.error(e)
         return jsonify({"message": "Get stock analysis failed"}), 500
