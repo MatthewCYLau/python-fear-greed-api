9b6249d3785034e5e2540dd39438cba0a90f112f
matthew.lau <matthew.lau@credera.co.uk>
2024-02-27 11:37:24 +0000

    spike: decode message before json load

--- api/analysis/views.py
+++ api/analysis/views.py
@@ -123,7 +123,7 @@ def handle_pubsub_subscription_push():
     pubsub_message = envelope["message"]
 
     if isinstance(pubsub_message, dict) and "data" in pubsub_message:
-        stock_symbol = pubsub_message["data"]["StockSymbol"]
+        stock_symbol = json.loads(pubsub_message["data"].decode("utf-8"))["StockSymbol"]
         logging.info(f"Received Pub Sub message with for stock {stock_symbol}.")
 
     return ("", 204)
