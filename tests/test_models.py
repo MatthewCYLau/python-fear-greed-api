0a05ce0fb45399ca6176d90395d84d3fec27a014
Matthew Lau <matthewlau@Matthews-MacBook-Pro.local>
2024-05-21 12:40:42 +0800

    feat: add tests on model

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from api.user.models import User
+from api.alert.models import Alert
 
 
 def test_new_user():
@@ -10,3 +12,13 @@ def test_new_user():
     )
     assert user.email == "foo@bar.com"
     assert user.name == "foo"
+
+
+def test_new_alert():
+    alert = Alert(
+        index=40,
+        note="foo",
+        created_by="bar",
+    )
+    assert alert.view_count == 0
+    assert type(alert.created) is datetime
