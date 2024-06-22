6035cbea8c1f18dfae638c106af2bd1ba5d18bd5
Matthew Lau <lau.cy.matthew@gmail.com>
2024-06-22 06:49:23 +0100

    chore: linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,4 +10,4 @@ def test_client():
 
     with flask_app.test_client() as testing_client:
         with flask_app.app_context():
-            yield testing_client 
+            yield testing_client
