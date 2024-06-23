a4af0736d66ca3ff735fcbc3ba067664571814f8
Matthew Lau <lau.cy.matthew@gmail.com>
2024-06-23 07:13:17 +0100

    feat: test random endpoint

--- tests/test_another_api.py
+++ tests/test_another_api.py
@@ -1,3 +1,5 @@
+import json
+
 def test_ping_with_fixture(test_client):
     response = test_client.get('/ping')
     assert response.status_code == 200
@@ -6,3 +8,9 @@ def test_ping_with_fixture(test_client):
 def test_not_found_with_fixture(test_client):
     response = test_client.get('/foo')
     assert response.status_code == 404
+
+def test_random_number_with_fixture(test_client):
+    response = test_client.get('/random')
+    assert response.status_code == 200
+    assert b"random_num" in response.data
+    assert isinstance(json.loads(response.data)['random_num'], int)
