f2b2a5a6724ea4598ea8accfcaa8110f3561391f
Matthew Lau <lau.cy.matthew@gmail.com>
2024-06-26 06:54:14 +0100

    feat: test export csv invalid date

--- tests/test_api_fixture_client.py
+++ tests/test_api_fixture_client.py
@@ -21,3 +21,9 @@ def test_async_with_fixture(test_client):
     response_json = json.loads(response.data)
     assert isinstance(response_json, list)
     assert isinstance(response_json[0]['result'], int)
+
+def test_records_export_csv_invalid_start_date(test_client):
+    start_date = "FOO"
+    response = test_client.post(f'/api/records/export-csv?startDate={start_date}&endDate=01-10-2023')
+    assert response.status_code == 400
+    assert b"Invalid date input" in response.data
